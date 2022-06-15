9bfe777d17cb0669e234e0b17397b1ab6c8e0d12
yantedesco <yantedesco@gmail.com>
2022-06-14 22:13:50 -0300

    requisição - axios

--- src/pages/Home/index.js
+++ src/pages/Home/index.js
@@ -1,20 +1,28 @@
 import { useState } from "react";
 import { useHistory } from "react-router-dom";
 import { Botao } from "../../components";
+import axios from "axios";
 
 export const Home = () => {
-  const [usuario] = useState({
-    nome: "Yan",
-    idade: 25
-  });
-
   const history = useHistory();
+  const [dado, setDado] = useState();
+
+  const pegarDados = async () => {
+    try {
+      const resposta = await axios.get("/users/yantedesco");
+      setDado(resposta.data);
+    } catch (erro) {
+      console.log(erro.message);
+    }
+  };
 
   return (
     <>
       <h1>HOME</h1>
+      <h2>{dado?.name}</h2>
       <Botao nome="Champions" onClick={() => history.push("/champions")} />
-      <Botao nome="Quem Somos" onClick={() => history.push(`/quem-somos/${usuario.nome}`)} />
+      <Botao nome="Quem Somos" onClick={() => history.push(`/quem-somos/`)} />
+      <Botao nome="Pegar Dados" onClick={pegarDados} />
     </>
   );
 };
